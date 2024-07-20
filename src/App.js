470a801644d897e5c79aa17da7a688ae85c30d41
anton <friendlyantz@pm.me>
2024-07-20 19:04:29 +1000

    INIT React app. render basic component and conditional styling

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+
+  const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+  ];
+
+  const listItems = products.map(
+    product =>
+      <li 
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'red' : 'green'
+      }}
+      >
+
+        {product.title}
+      </li>
+  )
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={user.imageUrl} className="App-logo" alt="logo" />
+          <h1>     {user.name} </h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        <ul>{listItems}</ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
